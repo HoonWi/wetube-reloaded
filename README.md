0d56972105a1a332b787a36982c89b9c39c2b2d1
HoonWi <wee0702@gmail.com>
2024-02-23 15:14:18 +0900

    update project setting - add routers & controller

---
+++ README.md
@@ -0,0 +1,15 @@
+#Wetube Reloader
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/user/edit -> Edit user
+/user/delete -> Delete user
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comment -> Comment on a video
+/videos/comments/delete -> Delete A Comment of a Video
--- src/server.js
+++ src/server.js
@@ -1,22 +1,20 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
+
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
-const handleHome = (req, res) => {
-  return res.send("ho");
-}
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
-const handleLogin = (req, res) => {
-  return res.send("yo");
-}
-
-app.get("/", handleHome);
-app.get("/login", handleLogin)
-
-const handleListening = () => { console.log(`Server listening on port http://localhost:${PORT}`)
-}
-
-
-app.listen(PORT, handleListening)
+  app.listen(PORT, handleListening);
