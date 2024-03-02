408131e27509dac762c21396389dabf85da3ab2c
HoonWi <wee0702@gmail.com>
2024-03-03 01:41:40 +0900

    template setting 완료

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,26 @@
-export const trending = (req, res) => res.render("home", {pageTitle: "Home"})
-export const see = (req, res) => res.render("watch")
-export const edit = (req, res) => res.send("Edit")
-export const search = (req, res) => res.send("Search")
-export const upload = (req, res) => res.send("upload")
-export const deleteVideo = (req, res) => res.send("deleteVideo")
+export const trending = (req, res) => {
+	const videos = [
+		{
+			title: "First",
+			rating: 5,
+			comment: 2,
+			createdAt: "2minutes ago",
+			views: 50,
+			id: 1,
+		},
+		{
+			title: "Second",
+			rating: 5,
+			comment: 2,
+			createdAt: "2minutes ago",
+			views: 50,
+			id: 2,
+		},
+	];
+	res.render("home", { pageTitle: "Home", videos });
+};
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.send("Edit");
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("upload");
+export const deleteVideo = (req, res) => res.send("deleteVideo");
